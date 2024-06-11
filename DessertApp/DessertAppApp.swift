178f2c37f2c324c6094447d618bdb5f7634aecda
Brandt Hofer <brandt.hofer@sciplay.com>
2024-06-10 23:10:23 -0500

    Initial commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Brandt Hofer on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
