2e43e703015ac0a70227e2877d6a5add8cc4c92d
Brandt Hofer <brandt.hofer@gmail.com>
2024-06-13 13:27:28 -0500

    restructuring for better organization

--- DessertApp/Views/MealDetailView.swift
+++ DessertApp/Views/MealDetailView.swift
@@ -1,40 +1,11 @@
 //
-//  ContentView.swift
+//  MealDetailView.swift
 //  DessertApp
 //
-//  Created by Brandt Hofer on 6/10/24.
+//  Created by Brandt Hofer on 6/13/24.
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    @StateObject private var viewModel = MenuViewModel()
-    
-    var body: some View {
-        NavigationView {
-            List(viewModel.meals) { meal in
-                NavigationLink(destination: MealDetailView(mealId: meal.id)) {
-                    HStack {
-                        AsyncImage(url: URL(string: meal.thumbnail)) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: 50, height: 50)
-                        .cornerRadius(8)
-                        Text(meal.name)
-                    }
-                }
-            }
-            .navigationTitle("Dessert Menu")
-            .onAppear {
-                viewModel.fetchMeals()
-            }
-        }
-    }
-}
 
 struct MealDetailView: View {
     let mealId: String
@@ -87,11 +58,5 @@ struct MealDetailView: View {
         .onAppear {
             viewModel.fetchMealDetail(id: mealId)
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
